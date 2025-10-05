ac46fa47ab13a4dea3590ada4d38727fb392ae7d
bairan <byerain009@gmail.com>
2025-10-05 20:14:35 +0530

    the model and repo is created

---
+++ product-inventory-api/src/main/java/com/practice/product_inventory_api/Product.java
@@ -0,0 +1,58 @@
+package com.practice.product_inventory_api;
+
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Table(name = "Product-Inventory")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+//    @NotNull(message= "Name can not be null")
+    private String name;
+    private double price;
+    private int stock;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Product(Long id, String name, double price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Product() {
+    }
+}
